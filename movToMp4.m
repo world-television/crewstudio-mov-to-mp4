d8a08f3f5bd3a538100738c906554835b94307b7
Tung Duong <duongtungls@gmail.com>
2022-07-08 14:59:04 +0700

    Fix issue with pass arg video quality for video convert

--- movToMp4.m
+++ movToMp4.m
@@ -12,76 +12,61 @@ RCT_EXPORT_MODULE()
 
 - (NSDictionary *)constantsToExport
 {
-  return @{
-    @"RNMov2Mp4ExportPreset": @{
-      @"ExportPresetLowQuality": @(ExportPresetLowQuality),
-      @"ExportPresetMediumQuality": @(ExportPresetMediumQuality),
-      @"ExportPresetHighestQuality": @(ExportPresetHighestQuality),
-      @"ExportPresetHEVCHighestQuality": @(ExportPresetHEVCHighestQuality),
-      @"ExportPresetHEVCHighestQualityWithAlpha": @(ExportPresetHEVCHighestQualityWithAlpha),
-      @"ExportPreset640x480": @(ExportPreset640x480),
-      @"ExportPreset960x540": @(ExportPreset960x540),
-      @"ExportPreset1280x720": @(ExportPreset1280x720),
-      @"ExportPreset1920x1080": @(ExportPreset1920x1080),
-      @"ExportPreset3840x2160": @(ExportPreset3840x2160),
-    },
-  };
+    if (@available(iOS 13.0, *)) {
+        return @{
+            @"RNMov2Mp4ExportPreset": @{
+                @"ExportPresetLowQuality": AVAssetExportPresetLowQuality,
+                @"ExportPresetMediumQuality": AVAssetExportPresetMediumQuality,
+                @"ExportPresetHighestQuality": AVAssetExportPresetHighestQuality,
+                @"ExportPresetHEVCHighestQuality": AVAssetExportPresetHEVCHighestQuality,
+                @"ExportPresetHEVCHighestQualityWithAlpha": AVAssetExportPresetHEVCHighestQualityWithAlpha,
+                @"ExportPreset640x480": AVAssetExportPreset640x480,
+                @"ExportPreset960x540": AVAssetExportPreset960x540,
+                @"ExportPreset1280x720": AVAssetExportPreset1280x720,
+                @"ExportPreset1920x1080": AVAssetExportPreset1920x1080,
+                @"ExportPreset3840x2160": AVAssetExportPreset3840x2160,
+            },
+        };
+    } else if (@available(iOS 11.0, *)) {
+            return @{
+                @"RNMov2Mp4ExportPreset": @{
+                    @"ExportPresetLowQuality": AVAssetExportPresetLowQuality,
+                    @"ExportPresetMediumQuality": AVAssetExportPresetMediumQuality,
+                    @"ExportPresetHighestQuality": AVAssetExportPresetHighestQuality,
+                    @"ExportPresetHEVCHighestQuality": AVAssetExportPresetHEVCHighestQuality,
+                    @"ExportPresetHEVCHighestQualityWithAlpha": AVAssetExportPresetHEVCHighestQuality,
+                    @"ExportPreset640x480": AVAssetExportPreset640x480,
+                    @"ExportPreset960x540": AVAssetExportPreset960x540,
+                    @"ExportPreset1280x720": AVAssetExportPreset1280x720,
+                    @"ExportPreset1920x1080": AVAssetExportPreset1920x1080,
+                    @"ExportPreset3840x2160": AVAssetExportPreset3840x2160,
+                },
+            };
+    } else {
+        return @{
+            @"RNMov2Mp4ExportPreset": @{
+                @"ExportPresetLowQuality": AVAssetExportPresetLowQuality,
+                @"ExportPresetMediumQuality": AVAssetExportPresetMediumQuality,
+                @"ExportPresetHighestQuality": AVAssetExportPresetHighestQuality,
+                @"ExportPresetHEVCHighestQuality": AVAssetExportPresetHighestQuality,
+                @"ExportPresetHEVCHighestQualityWithAlpha": AVAssetExportPresetHighestQuality,
+                @"ExportPreset640x480": AVAssetExportPreset640x480,
+                @"ExportPreset960x540": AVAssetExportPreset960x540,
+                @"ExportPreset1280x720": AVAssetExportPreset1280x720,
+                @"ExportPreset1920x1080": AVAssetExportPreset1920x1080,
+                @"ExportPreset3840x2160": AVAssetExportPreset3840x2160,
+            },
+        };
+    }
 }
 
 RCT_EXPORT_METHOD(convertMovToMp4: (NSString*)filename
                  toPath:(NSString*)outputPath
-                 quality:(NSInteger) quality
+                 videoQuality:(NSString*) videoQuality
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject
                 )
 {
-    
-    NSString* videoQuality = nil;
-    
-    switch (quality) {
-      case ExportPresetLowQuality:
-            videoQuality = AVAssetExportPresetLowQuality;
-            break;
-      case ExportPresetMediumQuality:
-            videoQuality = AVAssetExportPresetMediumQuality;
-            break;
-      case ExportPresetHighestQuality:
-            videoQuality = AVAssetExportPresetHighestQuality;
-            break;
-      case ExportPresetHEVCHighestQuality:
-            if (@available(iOS 11.0, *)) {
-                videoQuality = AVAssetExportPresetHEVCHighestQuality;
-            } else {
-                // Fallback on earlier versions
-                videoQuality = AVAssetExportPresetHighestQuality;
-            }
-            break;
-      case ExportPresetHEVCHighestQualityWithAlpha:
-            if (@available(iOS 13.0, *)) {
-                videoQuality = AVAssetExportPresetHEVCHighestQualityWithAlpha;
-            } else {
-                // Fallback on earlier versions
-                videoQuality = AVAssetExportPresetHighestQuality;
-            }
-            break;
-      case ExportPreset640x480:
-            videoQuality = AVAssetExportPreset640x480;
-            break;
-      case ExportPreset960x540:
-            videoQuality = AVAssetExportPreset960x540;
-            break;
-      case ExportPreset1280x720:
-            videoQuality = AVAssetExportPreset1280x720;
-            break;
-      case ExportPreset1920x1080:
-            videoQuality = AVAssetExportPreset1920x1080;
-            break;
-      case ExportPreset3840x2160:
-            videoQuality = AVAssetExportPreset3840x2160;
-            break;
-      default:
-            videoQuality = AVAssetExportPresetHighestQuality;
-    }
     
     NSURL *urlFile = [NSURL fileURLWithPath:filename];
     AVURLAsset *avAsset = [AVURLAsset URLAssetWithURL:urlFile options:nil];
