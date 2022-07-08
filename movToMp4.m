2b903c2bb949ab164ec92510d196e233219609f8
Duong Cong Tung <duongtungls@gmail.com>
2022-07-08 11:58:54 +0700

    Add option to convert to mp4 with quality (#1)

--- movToMp4.m
+++ movToMp4.m
@@ -10,18 +10,85 @@
 
 RCT_EXPORT_MODULE()
 
+- (NSDictionary *)constantsToExport
+{
+  return @{
+    @"RNMov2Mp4ExportPreset": @{
+      @"ExportPresetLowQuality": @(ExportPresetLowQuality),
+      @"ExportPresetMediumQuality": @(ExportPresetMediumQuality),
+      @"ExportPresetHighestQuality": @(ExportPresetHighestQuality),
+      @"ExportPresetHEVCHighestQuality": @(ExportPresetHEVCHighestQuality),
+      @"ExportPresetHEVCHighestQualityWithAlpha": @(ExportPresetHEVCHighestQualityWithAlpha),
+      @"ExportPreset640x480": @(ExportPreset640x480),
+      @"ExportPreset960x540": @(ExportPreset960x540),
+      @"ExportPreset1280x720": @(ExportPreset1280x720),
+      @"ExportPreset1920x1080": @(ExportPreset1920x1080),
+      @"ExportPreset3840x2160": @(ExportPreset3840x2160),
+    },
+  };
+}
+
 RCT_EXPORT_METHOD(convertMovToMp4: (NSString*)filename
                  toPath:(NSString*)outputPath
+                 quality:(NSInteger) quality
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject
                 )
 {
+    
+    NSString* videoQuality = nil;
+    
+    switch (quality) {
+      case ExportPresetLowQuality:
+            videoQuality = AVAssetExportPresetLowQuality;
+            break;
+      case ExportPresetMediumQuality:
+            videoQuality = AVAssetExportPresetMediumQuality;
+            break;
+      case ExportPresetHighestQuality:
+            videoQuality = AVAssetExportPresetHighestQuality;
+            break;
+      case ExportPresetHEVCHighestQuality:
+            if (@available(iOS 11.0, *)) {
+                videoQuality = AVAssetExportPresetHEVCHighestQuality;
+            } else {
+                // Fallback on earlier versions
+                videoQuality = AVAssetExportPresetHighestQuality;
+            }
+            break;
+      case ExportPresetHEVCHighestQualityWithAlpha:
+            if (@available(iOS 13.0, *)) {
+                videoQuality = AVAssetExportPresetHEVCHighestQualityWithAlpha;
+            } else {
+                // Fallback on earlier versions
+                videoQuality = AVAssetExportPresetHighestQuality;
+            }
+            break;
+      case ExportPreset640x480:
+            videoQuality = AVAssetExportPreset640x480;
+            break;
+      case ExportPreset960x540:
+            videoQuality = AVAssetExportPreset960x540;
+            break;
+      case ExportPreset1280x720:
+            videoQuality = AVAssetExportPreset1280x720;
+            break;
+      case ExportPreset1920x1080:
+            videoQuality = AVAssetExportPreset1920x1080;
+            break;
+      case ExportPreset3840x2160:
+            videoQuality = AVAssetExportPreset3840x2160;
+            break;
+      default:
+            videoQuality = AVAssetExportPresetHighestQuality;
+    }
+    
     NSURL *urlFile = [NSURL fileURLWithPath:filename];
     AVURLAsset *avAsset = [AVURLAsset URLAssetWithURL:urlFile options:nil];
 
     NSArray *compatiblePresets = [AVAssetExportSession exportPresetsCompatibleWithAsset:avAsset];
 
-    AVAssetExportSession *exportSession = [[AVAssetExportSession alloc] initWithAsset:avAsset presetName:AVAssetExportPresetMediumQuality];
+    AVAssetExportSession *exportSession = [[AVAssetExportSession alloc] initWithAsset:avAsset presetName:videoQuality];
 
     NSString * resultPath = [NSHomeDirectory() stringByAppendingFormat:@"/Documents/%@.mp4", outputPath];
 
